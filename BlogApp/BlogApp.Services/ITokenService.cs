a2960a6f957f175110c7bd160c4442aa06e968e7
boyadzhiev331 <a.boyadzhiev31@yahoo.com>
2021-09-06 12:49:37 +0300

    add services

---
+++ BlogApp/BlogApp.Services/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using BlogApp.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp.Services
+{
+    public interface ITokenService
+    {
+        public string CreateToken(ApplicationUserIdentity user);
+    }
+}
