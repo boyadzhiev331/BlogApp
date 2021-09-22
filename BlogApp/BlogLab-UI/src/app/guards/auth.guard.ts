b2b08efef5e539693d10817ba95b0d22f9180613
boyadzhiev331 <a.boyadzhiev31@yahoo.com>
2021-09-22 11:11:31 +0300

    quick fix auth guard

--- BlogApp/BlogLab-UI/src/app/guards/auth.guard.ts
+++ BlogApp/BlogLab-UI/src/app/guards/auth.guard.ts
@@ -17,7 +17,7 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
 
-      if (this.accountService.isLoggedIn) {
+      if (this.accountService.isLoggedIn()) {
         return true;
       }
 
